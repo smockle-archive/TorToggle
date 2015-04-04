d09f9fbcb692d5cc7c4c9faf369fc66b2bc576eb
Clay Miller <clay@smockle.com>
2015-04-03 17:21:41 -0700

    Initial Commit

---
+++ TorToggle/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  TorToggle
+//
+//  Created by Clay Miller on 4/3/15.
+//  Copyright (c) 2015 Clay Miller. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
