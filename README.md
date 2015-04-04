f78a6f4709be98f55bbfba72a395089486f49714
Clay Miller <clay@smockle.com>
2015-04-04 01:21:25 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+TorToggle
+=========
+An OS X menubar app that toggles a SOCKS proxy on and off.
+
+
+Pre-Requisites
+==============
+`brew install tor`
+`ln -sfv /usr/local/opt/tor/*.plist ~/Library/LaunchAgents`
+`launchctl load ~/Library/LaunchAgents/homebrew.mxcl.tor.plist`
+`networksetup -setsocksfirewallproxy Wi-Fi 127.0.0.1 9050 on`
+
+More information about configuring Tor [here](http://ramonski.de/blog/2012/07/03/using-tor-on-mac/) and [here](http://leonid.shevtsov.me/en/an-easy-way-to-use-tor-on-os-x).
+
+
+Disclaimer
+==========
+This product is produced independently from the Tor® anonymity software and carries no guarantee from The Tor Project about quality, suitability or anything else. For more information, please visit https://www.torproject.org/.
--- TorToggle/AppDelegate.swift
+++ TorToggle/AppDelegate.swift
@@ -12,16 +12,58 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
+    @IBOutlet weak var statusMenu: NSMenu!
 
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
 
+    func updateStatusMenuState() {
+        /* Check whether Tor is already enabled */
+        let task = NSTask()
+        let pipe = NSPipe()
+        task.launchPath = "/usr/sbin/networksetup"
+        task.arguments = ["-getsocksfirewallproxy", "Wi-Fi"]
+        task.standardOutput = pipe
+        task.launch()
+        
+        /* Show disabled icon variant if Tor is disabled */
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        let string = NSString(data: data, encoding: NSUTF8StringEncoding) as String
+        let array = string.componentsSeparatedByString("\n").filter { ($0 as NSString).containsString("Enabled:") && !($0 as NSString).containsString(" Enabled:") }
+        let disabled = array[0].lowercaseString == "enabled: no"
+        statusItem.button?.appearsDisabled = disabled
+        
+        /* Modify menu item text if Tor is disabled */
+        if (disabled) {
+            (statusMenu.itemArray[0] as NSMenuItem).title = "Enable Tor"
+        } else {
+            (statusMenu.itemArray[0] as NSMenuItem).title = "Disable Tor"
+        }
+    }
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        let icon = NSImage(named: "statusIcon")
+        icon?.setTemplate(true)
+        
+        statusItem.image = icon
+        statusItem.menu = statusMenu
+        
+        updateStatusMenuState()
     }
 
-    func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
+    @IBAction func menuClicked(sender: NSMenuItem) {
+        let task = NSTask()
+        task.launchPath = "/usr/sbin/networksetup"
+        
+        if (sender.title == "Disable Tor") {
+            task.arguments = ["-setsocksfirewallproxystate", "Wi-Fi", "off"]
+        } else {
+            task.arguments = ["-setsocksfirewallproxystate", "Wi-Fi", "on"]
+        }
+        
+        task.launch()
+        task.waitUntilExit()
+        
+        updateStatusMenuState()
     }
-
-
 }
 
